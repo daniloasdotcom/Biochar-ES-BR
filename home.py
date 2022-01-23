8b1eb2f356d394994d29a13774591fb18f0d951f
Danilo Andrade <danilo_as@live.com>
2022-01-23 15:18:38 -0300

    Enviado dos primeiros arquivos

--- home.py
+++ home.py
@@ -2,16 +2,17 @@ import streamlit as st
 import pages.projetos.projeto01 as PageProjetos01
 import pages.projetos.projeto02 as PageProjetos02
 import pages.projetos.papers as papers
+import pages.projetos.sprints as sprints
 
 pj1 = "Experimento 01"
 pj2 = "Experimento 02"
 pj3 = "Artigos Publicados"
+pj4 = "Tarefas da semana"
 
 
 st.sidebar.title('Menu')
 page_projeto = st.sidebar.selectbox('Escolha o Projeto de Pesquisa',
-                                    [pj1, pj2, pj3])
-
+                                    [pj1, pj2, pj3, pj4])
 
 def Choice():
     if page_projeto == pj1:
@@ -23,5 +24,7 @@ def Choice():
     elif page_projeto == pj3:
         papers.papers()
 
+    elif page_projeto == pj4:
+        sprints.sprints()
 
 Choice()
---
+++ pages/projetos/sprints.py
@@ -0,0 +1,98 @@
+import streamlit as st
+
+
+def sprints():
+
+    st.markdown("""
+    ### Segunda-feira
+    - **No computador**:
+        - Ajustar pedência da prestação de contas com a FAPES
+        - Enviar para o professor Renato a ficha de avaliação
+        - Enviar para o professor Renato os slides do curso de Biocarvão
+        - Solicitar ao Professor Renato um arquivo com o passo a passo para uso do reator
+        - Organizar um plano para determinação de Frações óxidáveis do carbono para o Bruno
+        - Solicitar ao professor Otacílio apoio no processo de limpeza da área do experimento
+        - Comunicar aos bolsista meus horários no laboratório e tarefas a serem realizadas
+        - Avisar a Camila sobre o nível de disponibilidade durante a semana e sobre o problema com a escada
+        - *Publicações*:
+            - Organizar o processo de readequação do artigo do mestrado   
+             
+        
+    - **No laboratório**:
+        - Organizar o matérial para determinação de frações óxidáveis do carbono para o Bruno
+        
+        
+    - **No computador**:
+        - Realizar os ajustes de tarefas da semana e futuras
+            
+    """, unsafe_allow_html=True)
+
+    st.markdown("""
+        ### Terça-feira
+        - **No camputador**:
+            - Comunicar aos bolsista meus horários no laboratório e tarefas a serem realizadas
+            - *Publicações*:
+            
+
+        - **No laboratório**:
+            - Iniciar a determinação de frações óxidáveis do carbono para o Bruno
+            - Organizar a salinha química
+            - Verificar funcionamento do extrator de Richards 
+            
+             
+        - **No computador**:
+            - Realizar os ajustes de tarefas da semana e futuras
+
+        """, unsafe_allow_html=True)
+
+    st.markdown("""
+        ### Quarta-feira
+        - **No computador**:
+            - *Publicações*:
+        
+
+        - **No laboratório**:
+            - Finalizar a determinação de frações óxidáveis do carbono para o Bruno
+            
+            
+        - **No computador**:
+            - Realizar os ajustes de tarefas da semana e futuras    
+            - enviar resultados ao Bruno e comunicado ao Professor Otacílio
+            - comunicar aos bolsistas meus horários no laboratório e tarefas a serem realizadas
+
+        """, unsafe_allow_html=True)
+
+    st.markdown("""
+        ### Quinta-feira
+        - **No computador**:
+            - *Publicações*:
+        
+
+        - **Na área experimental**:
+            - Primeira fornada de biocarvão
+            - Organização do galpão
+            
+            
+        - **No laboratório**:
+            - Orientação ao pessoal sobre procedimentos com os agregados
+            
+            
+        - **No computador**:
+            - Realizar os ajustes de tarefas da semana e futuras   
+
+        """, unsafe_allow_html=True)
+
+    st.markdown("""
+        ### Sexta-feira
+        - **No computador**:
+            - *Publicações*:
+            
+
+        - **Na área experimental**:
+            - Finalizar a produção da primeira fornada de biocarvão
+            
+            
+        - **No computador**:
+            - Realizar os ajustes de tarefas da semana e futuras   
+
+        """, unsafe_allow_html=True)

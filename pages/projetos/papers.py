f58b99d40025cb176363d38f77b75d2a19728147
Danilo Andrade <danilo_as@live.com>
2022-01-24 18:30:46 -0300

    Envia 24/01/2022

--- pages/projetos/papers.py
+++ pages/projetos/papers.py
@@ -19,12 +19,26 @@ def papers():
 
     st.markdown(text02, unsafe_allow_html=True)
 
+    text03 = "<p style='text-align: justify;'> Santos, D. A.; Passos, R. R.; Faé Sartori, A.; Zacarias, A.; Silva, " \
+             "C.; Passos Rangel, O.; Mosa, L., Profeti, D., Profeti, L., Silva, R., 2021. Biocarvão de casca de " \
+             "eucalipto efeito sobre atributos químicos de um latossolo vermelho-amarelo cultivado com eucaliptus " \
+             "urograndis. https://doi.org/10.37885/210605169 </p> "
+
+    st.markdown(text03, unsafe_allow_html=True)
+
+    text04 = "<p style='text-align: justify;'> Passos, R., Andrade Santos, D., Faé Sartori, A., Zacarias, A., " \
+             "Mendonça, I., Passos Rangel, O., Profeti, D., Profeti, L., Silva, R., 2021. Carbono orgânico e " \
+             "atributos físicos de um latossolo vermelho-amarelo sob aplicação de biocarvão cultivado com Eucalyptus " \
+             "Urograndis. https://doi.org/10.37885/210605170 </p> "
+
+    st.markdown(text04, unsafe_allow_html=True)
+
     st.write("""
         ## Artigos
         """)
 
-    text03 = "<p style='text-align: justify;'> FONSECA, A.A.; SANTOS, D. A.; Moura Junior, C.D.; PASSOS, " \
+    text05 = "<p style='text-align: justify;'> FONSECA, A.A.; SANTOS, D. A.; Moura Junior, C.D.; PASSOS, " \
              "R. R.; Rangel, O.J.P. Phosphorus and potassium in aggregates of degraded soils: changes caused by " \
              "biochar application. CLEAN (WEINHEIM. INTERNET), p. 2000366, 2021. </p> "
 
-    st.markdown(text03, unsafe_allow_html=True)
+    st.markdown(text05, unsafe_allow_html=True)

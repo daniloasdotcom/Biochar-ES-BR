9414fc0827a494c4e04f4be575eee9cb09865892
Danilo Andrade <danilo_as@live.com>
2022-05-19 13:41:24 -0300

    Envio 19/05/2022

--- pages/projetos/papers.py
+++ pages/projetos/papers.py
@@ -55,8 +55,8 @@ def papers():
         ## Artigos
         """)
 
-    text06 = "<p style='text-align: justify;'> FONSECA, A. A.; SANTOS, D. A.; Moura Junior, C.D.; PASSOS, " \
-             "R. R.; Rangel, O.J.P. Phosphorus and potassium in aggregates of degraded soils: changes caused by " \
+    text06 = "<p style='text-align: justify;'> FONSECA, A. A.; SANTOS, D. A.; MOURA JUNIOR, C.D.; PASSOS, " \
+             "R. R.; RANGEL, O.J.P. Phosphorus and potassium in aggregates of degraded soils: changes caused by " \
              "biochar application. <strong>CLEAN</strong> (WEINHEIM. INTERNET), p. 2000366, 2021. </p> "
 
     st.markdown(text06, unsafe_allow_html=True)

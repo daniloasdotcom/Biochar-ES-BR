4c3596137bc0e55742c93b6229695d86353c518c
Danilo Andrade <danilo_as@live.com>
2022-06-01 06:39:06 -0300

    Envio 01/06/2022

--- pages/projetos/projeto01.py
+++ pages/projetos/projeto01.py
@@ -16,11 +16,11 @@ def Projeto01():
     """)
 
     text02 = "<p style='text-align: justify;'> Este experimento de campo é também uma continuação de um ensaio " \
-             "estabelecido, em março de 2018 com recursos do EDITAL FAPES/SEAG Nº 06/2015, na área experimental do " \
+             "estabelecido em março de 2018 com recursos do EDITAL FAPES/SEAG Nº 06/2015, na área experimental do " \
              "IFES - Campus de Alegre. Neste ensaio os biocarvões de casca de eucalipto são testados em tratamentos " \
              "distribuídos num delineamento em blocos casualizados em um esquema fatorial 5 x 2, sendo cinco doses de " \
              "biocarvões (0; 0,25%; 0,5%; 1,0% e 2% em volume de biocarvões por volume de sulco) produzidos sob duas " \
-             "temperaturas (350 ºc e 600 ºc).</p> "
+             "temperaturas (350ºC e 600ºC).</p> "
 
     st.markdown(text02, unsafe_allow_html=True)
 

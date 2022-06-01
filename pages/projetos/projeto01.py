04e133ce2a77b6e66d7b69805b554580d39c6150
Danilo Andrade <danilo_as@live.com>
2022-06-01 06:52:55 -0300

    Envio 01/06/2022

--- pages/projetos/projeto01.py
+++ pages/projetos/projeto01.py
@@ -1,9 +1,16 @@
 import streamlit as st
+from PIL import Image
+
 
 def Projeto01():
-    text01 = "<h1 style='text-align: center; line-height: 1.15'> Monitoramento de atributos do solo, " \
-             "desenvolvimento e estado " \
-             "nutricional de plantas de eucalipto em campo, sob aplicação de biocarvões</h1> "
+    col1, col2, col3 = st.columns([1, 6, 1])
+
+    img_biochar02 = Image.open("images/plantioEucalipto.png")
+
+    col2.image(img_biochar02, width=1920, use_column_width=True)
+
+    text01 = "<h1 style='text-align: center; line-height: 1.15'> Monitoring of soil attributes, development and " \
+             "nutritional status of eucalyptus plants in the field, under application of biochars</h1> "
 
     st.markdown(text01, unsafe_allow_html=True)
 
@@ -15,12 +22,12 @@ def Projeto01():
     ## Resumo
     """)
 
-    text02 = "<p style='text-align: justify;'> Este experimento de campo é também uma continuação de um ensaio " \
-             "estabelecido em março de 2018 com recursos do EDITAL FAPES/SEAG Nº 06/2015, na área experimental do " \
-             "IFES - Campus de Alegre. Neste ensaio os biocarvões de casca de eucalipto são testados em tratamentos " \
-             "distribuídos num delineamento em blocos casualizados em um esquema fatorial 5 x 2, sendo cinco doses de " \
-             "biocarvões (0; 0,25%; 0,5%; 1,0% e 2% em volume de biocarvões por volume de sulco) produzidos sob duas " \
-             "temperaturas (350ºC e 600ºC).</p> "
+    text02 = "<p style='text-align: justify;'> In March 2018, a field trial was started using resources from EDITAL " \
+             "FAPES/SEAG Nº 06/2015, in the experimental area of IFES - Campus de Alegre - Brazil. In this assay, " \
+             "eucalyptus bark biochars are tested in treatments distributed in a randomized block design in a 5 x 2 " \
+             "factorial scheme, with five doses of biochars (0; 0.25%; 0.5%; 1.0% and 2 % by volume of biochars by " \
+             "volume of furrow) produced under two temperatures (350ºC and 600ºC). The trial is now maintained with " \
+             "resources from Fapes/Cnpq Nº 11/2019 - Regional technological and scientific development program - " \
+             "PDCTR 2019.</p> "
 
     st.markdown(text02, unsafe_allow_html=True)
-

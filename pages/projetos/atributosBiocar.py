6957512d872e6f17cc46055eecc35cfbb8442d26
Danilo Andrade <danilo_as@live.com>
2022-06-02 19:21:24 -0300

    Envio 02/06/2022

--- pages/projetos/atributosBiocar.py
+++ pages/projetos/atributosBiocar.py
@@ -7,12 +7,7 @@ def atributos():
     st.title("Caracterização dos biocarvões")
     st.markdown("Os gráficos abaixo ajudam a visualizar as atributos dos biocarvões")
 
-    col1, col2 = st.columns([1, 1])
-
-    chart_visual = col1.selectbox('Select Charts/Plot type',
-                                        ('Bar Chart', 'Bubble Chart'))
-
-    selected_status = col2.selectbox('Select o atributo desejado',
+    selected_status = st.selectbox('Select o atributo desejado',
                                            options=['pH',
                                                     'Cálcio',
                                                     'Magnésio',
@@ -22,74 +17,28 @@ def atributos():
 
     fig02 = plt.figure(figsize=(10, 5))
 
-    if chart_visual == 'Bar Chart':
-        if selected_status == 'pH':
-            plt.bar(data.biocar, data.pH)
-            plt.title('Gráfico de Progresso das Atividades da Sprint')
-            plt.xlabel('Tempo')
-            plt.ylabel('Progresso')
-            plt.grid(True)
-            plt.legend(loc="upper left")
-            plt.show()
+    if selected_status == 'pH':
+        plt.bar(data.biocar, data.pH)
+        plt.xlabel('Biocarvão')
+        plt.ylabel('pH')
+        plt.show()
 
-            st.pyplot(fig02)
+    elif selected_status == 'Cálcio':
+        plt.bar(data.biocar, data.Ca)
+        plt.xlabel('Biocarvão')
+        plt.ylabel('Cálcio (dag/kg)')
+        plt.show()
 
-        if selected_status == 'Cálcio':
-            fig.add_trace(go.Bar(x=data.biocar,
-                                 y=data.Ca))
-            fig.update_xaxes(showline=True, linewidth=2, linecolor='black')
-            fig.update_yaxes(showline=True, linewidth=2, linecolor='black', ticks="outside", tickwidth=2)
-            fig.update_layout(xaxis_title="Biocarvões", yaxis_title="Cálcio (dag/kg)")
+    elif selected_status == 'Magnésio':
+        plt.bar(data.biocar, data.Mg)
+        plt.xlabel('Biocarvão')
+        plt.ylabel('Magnésio (dag/kg)')
+        plt.show()
 
-        if selected_status == 'Magnésio':
-            fig.add_trace(go.Bar(x=data.biocar,
-                                 y=data.Mg))
-            fig.update_xaxes(showline=True, linewidth=2, linecolor='black')
-            fig.update_yaxes(showline=True, linewidth=2, linecolor='black', ticks="outside", tickwidth=2)
-            fig.update_layout(xaxis_title="Biocarvões", yaxis_title="Magnésio (dag/kg)")
+    elif selected_status == 'Fósforo':
+        plt.bar(data.biocar, data.P)
+        plt.xlabel('Biocarvão')
+        plt.ylabel('Fósforo (dag/kg)')
+        plt.show()
 
-        if selected_status == 'Fósforo':
-            fig.add_trace(go.Bar(x=data.biocar,
-                                 y=data.P))
-            fig.update_xaxes(showline=True, linewidth=2, linecolor='black')
-            fig.update_yaxes(showline=True, linewidth=2, linecolor='black', ticks="outside", tickwidth=2)
-            fig.update_layout(xaxis_title="Biocarvões", yaxis_title="Fósforo (dag/kg)")
-
-    elif chart_visual == 'Bubble Chart':
-        if selected_status == 'pH':
-            fig.add_trace(go.Scatter(x=data.biocar,
-                                     y=data.pH,
-                                     mode='markers',
-                                     marker_size=[40, 60, 80, 60, 40, 50]))
-            fig.update_xaxes(showline=True, linewidth=2, linecolor='black')
-            fig.update_yaxes(showline=True, linewidth=2, linecolor='black', ticks="outside", tickwidth=2)
-            fig.update_layout(xaxis_title="Biocarvões", yaxis_title="pH")
-
-        if selected_status == 'Cálcio':
-            fig.add_trace(go.Scatter(x=data.biocar,
-                                     y=data.Ca,
-                                     mode='markers',
-                                     marker_size=[40, 60, 80, 60, 40, 50]))
-            fig.update_xaxes(showline=True, linewidth=2, linecolor='black')
-            fig.update_yaxes(showline=True, linewidth=2, linecolor='black', ticks="outside", tickwidth=2)
-            fig.update_layout(xaxis_title="Biocarvões", yaxis_title="Cálcio (dag/kg)")
-
-        if selected_status == 'Magnésio':
-            fig.add_trace(go.Scatter(x=data.biocar,
-                                     y=data.Mg,
-                                     mode='markers',
-                                     marker_size=[40, 60, 80, 60, 40, 50]))
-            fig.update_xaxes(showline=True, linewidth=2, linecolor='black')
-            fig.update_yaxes(showline=True, linewidth=2, linecolor='black', ticks="outside", tickwidth=2)
-            fig.update_layout(xaxis_title="Biocarvões", yaxis_title="Magnésio (dag/kg)")
-
-        if selected_status == 'Fósforo':
-            fig.add_trace(go.Scatter(x=data.biocar,
-                                     y=data.P,
-                                     mode='markers',
-                                     marker_size=[40, 60, 80, 60, 40, 50]))
-            fig.update_xaxes(showline=True, linewidth=2, linecolor='black')
-            fig.update_yaxes(showline=True, linewidth=2, linecolor='black', ticks="outside", tickwidth=2)
-            fig.update_layout(xaxis_title="Biocarvões", yaxis_title="Fósforo (dag/kg)")
-
-    st.plotly_chart(fig, use_container_width=True)
+    st.pyplot(fig02)

0bb195f220108122c0e55438458e82e10b3a14e9
Danilo Andrade <danilo_as@live.com>
2022-06-10 15:42:08 -0300

    Envio 10/06/2022

--- pages/projetos/sprints.py
+++ pages/projetos/sprints.py
@@ -15,7 +15,7 @@ def sprints():
     time = ["02/06", "03/06", "04/06", "05/06", "06/06", "07/06", "08/06",
             "09/06", "10/06", "11/06", "12/06", "13/06", "14/06", "15/06"]
     progress_desired = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
-    real_progress =    [1, 1, 3, 3, 5, 6, 7, 8, 8,  8,  8,  8,  8,  8]
+    real_progress =    [1, 1, 3, 3, 5, 6, 7, 8, 9,  9,  9,  9,  9,  9]
 
     fig02 = plt.figure(figsize=(10, 5))
 
@@ -106,20 +106,20 @@ def sprints():
     - ~~Atualizar as atividades e o gráfico~~
     
     ### Day 10/06 - Sexta-feira
-    - Trabalhar nos textos dos projetos
-    - Avaliar os cálculos realizados pelo Mateus com os dados de 4 anos
-    - Enviar proposta de trabalho para o próximo ciclo da iniciação
-    - Solicitar aos IC's o cronograma para o periódo de 20 de junho a 01 de agosto
-    - Alocar as reuniões da sprint
-    - Atualizar as atividades e o gráfico
+    - ~~Avaliar os cálculos realizados pelo Mateus com os dados de 4 anos~~
+    - ~~Solicitar aos IC's o cronograma para o periódo de 20 de junho a 01 de agosto~~
+    - ~~Atualizar as atividades e o gráfico~~
     
     ### Day 11/06 - Sábado
+    - Alocar as reuniões da sprint
+    - Trabalhar nos textos dos projetos
     - Descanso
      
     ### Day 12/06 - Domingo
     - Descanso
     
     ### Day 13/06 - Segunda-feira
+    - Enviar proposta de trabalho para o próximo ciclo da iniciação
     - leitura de Ca e Mg com o Carlos
     - Extração de K, Na e P
     - Cachimbagem e extração para análise de H+Al (Preparar Fenolftaleina) 10-30 cm

a7ceb1e56f29bec2827873fd66eac77f0af22dac
Danilo Andrade <danilo_as@live.com>
2022-06-14 19:11:47 -0300

    Envio 13/06/2022

--- pages/projetos/sprints.py
+++ pages/projetos/sprints.py
@@ -15,7 +15,7 @@ def sprints():
     time = ["02/06", "03/06", "04/06", "05/06", "06/06", "07/06", "08/06",
             "09/06", "10/06", "11/06", "12/06", "13/06", "14/06", "15/06"]
     progress_desired = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
-    real_progress =    [1, 1, 3, 3, 5, 6, 7, 8, 9, 10, 11, 12, 12, 12]
+    real_progress =    [1, 1, 3, 3, 5, 6, 7, 8, 9, 10, 11, 11, 11, 14]
 
     fig02 = plt.figure(figsize=(10, 5))
 
@@ -49,6 +49,7 @@ def sprints():
         - Produção dos textos de PIIC
         - Avaliação dos dados da Duda
         - Determinação da estabilidade de agregados após 4 anos
+        - Cachimbagem e extração para análise de H+Al (Preparar Fenolftaleina) 10-30 cm
         """)
 
 
@@ -119,27 +120,25 @@ def sprints():
     ### Day 13/06 - Segunda-feira
     - ~~Leitura de Ca e Mg com o Carlos~~
     - ~~Extração de K, Na e P~~
+    - Envio do Cronograma solicitado aos IC's
     - ~~Limpeza dos materiais~~
     - ~~Atualizar as atividades e o gráfico~~
     
     ### Day 14/06 - Terça-feira
-    - Determinação de K, Na e P
-    - Trabalhar nos textos dos projetos
-    - Envio do Cronograma solicitado aos IC's
-    - Trabalhar no extrator de Richards
-    - Alocar as atividades da sprint
-    - Alocar as reuniões da sprint
+    - ~~Determinação de K, Na e P~~
+    - ~~Trabalhar nos textos dos projetos~~
     - Atualizar as atividades e o gráfico
     
     ### Day 15/06 - Quarta-feira
-    - Organizar os dados de Física do Solo da Duda
-    - Criar uma lista de evetnos de divulgação
-    - Enviar proposta de trabalho para o próximo ciclo da iniciação
+    - Trabalhar no extrator de Richards
+    - Criar uma lista de eventos de divulgação dos nossos trabalhos
+    - Enviar para o Renato a proposta de trabalho para o próximo ciclo da iniciação
     - Aniversário do Ueslei
+    - Organizar os dados de Física do Solo da Duda
     - Enviar avaliação de desenpenho aos IC's
     - Preparar e enviar o cronograma para cada um dos IC's o periódo de 20 de junho a 01 de agosto
-    - Cachimbagem e extração para análise de H+Al (Preparar Fenolftaleina) 10-30 cm
     - Alocar as atividades da sprint
+    - Alocar as reuniões da sprint
     - Atualizar as atividades e o gráfico
             
     """, unsafe_allow_html=True)

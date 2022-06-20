eac75cf2dad4913ea5b31a786cd7f4d42a03e82c
Danilo Andrade <danilo_as@live.com>
2022-06-20 18:51:45 -0300

    Envio 20/06/2022

--- pages/projetos/sprints.py
+++ pages/projetos/sprints.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import matplotlib.pyplot as plt
 import datetime
-import calendar
 
 def sprints():
 
@@ -12,14 +11,10 @@ def sprints():
 
     st.markdown(text01, unsafe_allow_html=True)
 
-    year = 2022
-    month = 6
-    print(calendar.month(year, month))
-
     time = ["20/06", "21/06", "22/06", "23/06", "24/06",
             "27/06", "28/06", "29/06", "30/06", "01/06"]
     progress_desired = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    real_progress =    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    real_progress =    [1, 1, 1, 1, 1, 1, 1, 1, 1,  1]
 
     fig02 = plt.figure(figsize=(10, 5))
 
@@ -48,23 +43,14 @@ def sprints():
         - Análises foliares
         - Medição das plantas
         - Masseração do solo para análise de COT e NT
-        - Trabalhar no extrator de Richards
-        - Criar uma lista de eventos de divulgação dos nossos trabalhos
-        - Enviar para o Renato a proposta de trabalho para o próximo ciclo da iniciação
-        - Aniversário do Ueslei
         - Alocar as reuniões da sprint
-        - Atualizar as atividades e o gráfico
+        
         """)
 
     with col2.expander("Demais"):
         st.markdown("""
-        - Determinação da CC e PMP
-        - Produção dos textos de PIIC
-        - Avaliação dos dados da Duda
-        - Determinação da estabilidade de agregados após 4 anos
-        - Cachimbagem e extração para análise de H+Al (Preparar Fenolftaleina) 10-30 cm
-        - Organizar os dados de Física do Solo da Duda
-        - Enviar avaliação de desenpenho aos IC's
+        
+        - Criar uma lista de eventos de divulgação dos nossos trabalhos
         - Preparar e enviar o cronograma para cada um dos IC's o periódo de 20 de junho a 01 de agosto
         - Alocar as atividades da sprint
         
@@ -74,16 +60,25 @@ def sprints():
                 # To do
     """)
 
-    with st.expander("Day 20/06"):
+    with st.expander("Day 20/06 - Concluído"):
 
-        st.markdown("""    
-            
-        - Atualizar as atividades e o gráfico
+        st.markdown("""
+        
+        - ~~Alocar atividades da sprint~~
+        - ~~Finalizar a proposta de trabalho para o próximo ciclo da iniciação~~       
+        - ~~Atualizar as atividades e o gráfico~~
             
         """)
     with st.expander("Day 21/06"):
         st.markdown("""    
 
+        - Aniversário do Ueslei
+        - Avaliação dos dados da Duda
+        - Organizar um quadro de horários para o Ueslei
+        - Trabalhar no extrator de Richards
+        - Fazer anotações de desempenho dos IC's
+        - Cachimbagem e para análise de H+Al (Preparar Fenolftaleina) 10-30 cm de 3 anos
+        - Organização da salinha de estudos
         - Atualizar as atividades e o gráfico
 
         """)
@@ -91,6 +86,7 @@ def sprints():
     with st.expander("Day 22/06"):
         st.markdown("""    
 
+        - Fazer anotações de desempenho dos IC's
         - Atualizar as atividades e o gráfico
 
         """)
@@ -98,6 +94,7 @@ def sprints():
     with st.expander("Day 23/06"):
         st.markdown("""    
 
+        - Fazer anotações de desempenho dos IC's
         - Atualizar as atividades e o gráfico
 
         """)
@@ -105,6 +102,7 @@ def sprints():
     with st.expander("Day 24/06"):
         st.markdown("""    
 
+        - Fazer anotações de desempenho dos IC's
         - Atualizar as atividades e o gráfico
 
         """)
@@ -112,6 +110,9 @@ def sprints():
     with st.expander("Day 27/06"):
         st.markdown("""    
 
+        - Determinação da CC e PMP
+        - Separar agregados do solo de 4 anos
+        - Fazer anotações de desempenho dos IC's
         - Atualizar as atividades e o gráfico
 
         """)
@@ -120,12 +121,15 @@ def sprints():
         st.markdown("""    
 
         - Atualizar as atividades e o gráfico
+        - Fazer anotações de desempenho dos IC's
+        - Determinação da estabilidade de agregados após 4 anos
 
         """)
 
     with st.expander("Day 29/06"):
         st.markdown("""    
 
+        - Fazer anotações de desempenho dos IC's
         - Atualizar as atividades e o gráfico
 
         """)
@@ -133,6 +137,7 @@ def sprints():
     with st.expander("Day 30/06"):
         st.markdown("""    
 
+        - Fazer anotações de desempenho dos IC's
         - Atualizar as atividades e o gráfico
 
         """)
@@ -140,6 +145,8 @@ def sprints():
     with st.expander("Day 01/07"):
         st.markdown("""    
 
+        - Enviar avaliação de desenpenho aos IC's
+        - Fazer anotações de desempenho dos IC's
         - Atualizar as atividades e o gráfico
 
         """, unsafe_allow_html=True)

24b74d6c8caa0ea5b015b6abb19860cde4ba7594
Danilo Andrade <danilo_as@live.com>
2023-01-06 08:47:07 -0300

    Atualização das publicações

--- pages/04_🧪_Papers.py
+++ pages/04_🧪_Papers.py
@@ -233,5 +233,27 @@ def papers():
                            file_name="Tese_Ronaldo_2021.pdf",
                            mime='application/octet-stream')
 
+    st.write("""# Resumes""")
+    # Primeiro Resumo
+    with st.container():
+        text09 = "<p style='text-align: justify;'>Caracterização de biocarvões, estudo de sua interação com o ânion " \
+                 "P e disponibilidade de P no solo. In: Anais de Iniciação Científica da UFES, 2020. Anais [...] " \
+                 "Vitória: UFES. 2020.</p> "
+
+        st.markdown(text09, unsafe_allow_html=True)
+
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        if st.button('Read PDF', key='22'):
+            show_pdf('files/IC_Mateus_2020.pdf')
+    with col2:
+        st.button('Close PDF', key='23')
+    with col3:
+        with open("files/IC_Mateus_2020.pdf", "rb") as pdf_file:
+            PDFbyte = pdf_file.read()
+        st.download_button(label="Download PDF", key='24',
+                           data=PDFbyte,
+                           file_name="IC_Mateus_2020.pdf",
+                           mime='application/octet-stream')
 
 papers()

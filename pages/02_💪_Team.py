ca5982de3b0f117be1e032d0f249d7e0f1e66715
Danilo Andrade <danilo_as@live.com>
2024-10-06 18:12:41 -0300

    update06102024

--- pages/02_💪_Team.py
+++ pages/02_💪_Team.py
@@ -25,24 +25,22 @@ def team():
     st.sidebar.write("##")
     st.sidebar.write("##")
 
-    text01 = "<h1 style='text-align: center; line-height: 1.15'> Roles of our Scrum Team</h1> "
+    text01 = "<h1 style='text-align: center; line-height: 1.15'>Main Team</h1> "
 
     st.markdown(text01, unsafe_allow_html=True)
 
     st.markdown('')
     st.markdown('')
 
-    col1, col2, col3 = st.columns([1, 1, 1])
+    col1, col2 = st.columns([1, 1])
 
     # Primeira linha de imagens
     equipe01 = Image.open("images/danilo.png")
     equipe02 = Image.open("images/renato.png")
-    equipe03 = Image.open("images/amanda.png")
 
     # Inserção das imagens
     col1.image(equipe01, caption="Danilo - Coordinator, PO and Scrum Master")
     col2.image(equipe02, caption="Renato R. Passos -  Supervisor, Stakeholder")
-    col3.image(equipe03, caption="Amanda Gomes -  Volunteer Technical Support and Development Team")
 
     # Inserção da descrição
     with col1.expander("More about Danilo"):
@@ -53,37 +51,5 @@ def team():
     with col2.expander("More about Renato"):
         st.write("Você saberá mais sobre ele em breve aqui")
 
-    with col3.expander("More about Amanda"):
-        st.write("Você saberá mais sobre ela em breve aqui")
-
-    # Segunda linha de imagens
-    equipe04 = Image.open("images/mateus.png")
-    equipe05 = Image.open("images/duda.png")
-    equipe06 = Image.open("images/aurelio.png")
-
-    # Inserção das imagens
-    col1.image(equipe04, caption="Mateus - Junior Researcher ans Development Team")
-    col2.image(equipe05, caption="Maria Eduarda - Junior Researcher and Development Team")
-    col3.image(equipe06, caption="Aurélio - Junior Researcher and Development Team")
-
-    # Inserção da descrição
-    with col1.expander("More about Mateus"):
-        st.write("Você saberá mais sobre ele em breve aqui")
-
-    with col2.expander("More about Maria Eduarda"):
-        st.write("Você saberá mais sobre ela em breve aqui")
-
-    with col3.expander("More about Aurélio"):
-        st.write("Você saberá mais sobre ele em breve aqui")
-
-    # Terceira linha de imagens
-    equipe07 = Image.open("images/ueslei.png")
-
-    # Inserção das imagens
-    col1.image(equipe07, caption="Ueslei - Junior Researcher and Development Team")
-
-    # Inserção da descrição
-    with col1.expander("More about Ueslei"):
-        st.write("Você saberá mais sobre ele em breve aqui")
 
 team()
